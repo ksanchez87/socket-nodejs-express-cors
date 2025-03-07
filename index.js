b1246f73eea444ab19c818c3021e9d5c77a666f8
ksanchez87 <santiago23598@gmail.com>
2025-03-07 12:36:08 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const cors = require("cors");
+const http = require('http');
+const app = express();
+const {Server} = require("socket.io");
+
+const port = process.env.PORT || 3001;
+
+app.use(cors());
+app.use(express.json());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:5173",
+        methods: ["GET", "POST"]
+    }
+})
+
+io.on("connection", (socket) => {
+    console.log(`Usuario actual:${socket.id}`);
+
+    socket.on("join_room", (data) => {
+        socket.join(data);
+        console.log(`Usuario con id:${socket.id} se unio a la sala: ${data}`);
+    })
+
+    socket.on("send_message", (data) => {
+        socket.to(data.room).emit("received_message", data)
+    })
+
+    socket.on("disconnect", () => {
+        console.log("Usuario desconectado", socket.id);
+    })
+})
+
+server.listen(port, () =>{
+    console.log(`Server in port http://localhost:${port}`);
+})
